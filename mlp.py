805ccd90eea268daa45262dd90f32f619f3285d6
LeaYeh <smile2140@gmail.com>
2025-04-28 19:54:31 +0000

    chore: Setup project folder structure

---
+++ mlp.py
@@ -0,0 +1,9 @@
+import numpy as np
+import pandas as pd
+from litetorch.nn.sequential import Sequential
+from litetorch.nn.linear import Linear
+from litetorch.nn.activation import ReLU
+from litetorch.loss import MSELoss
+from litetorch.optim import SGD
+
+
