51556872875c00d54ce6fe77bf9bf081dc58fbf4
LeaYeh <smile2140@gmail.com>
2025-05-09 16:54:30 +0000

    refactor: Split to train and test only in the first stage

--- mlp.py
+++ mlp.py
@@ -1,13 +1,11 @@
 import typer
 from src.multilayer_perceptron.split import split_cli
-from src.multilayer_perceptron.model import model_cli
 from src.multilayer_perceptron.train import train_cli
 from src.multilayer_perceptron.predict import predict_cli
 
 
 app = typer.Typer()
 app.add_typer(split_cli, name="split")
-app.add_typer(model_cli, name="model")
 app.add_typer(train_cli, name="train")
 app.add_typer(predict_cli, name="predict")
 
--- src/multilayer_perceptron/split.py
+++ src/multilayer_perceptron/split.py
@@ -2,7 +2,7 @@ import os
 import typer
 import numpy as np
 import pandas as pd
-from litetorch.data.split import train_val_test_split
+from litetorch.data.split import train_test_split
 
 
 split_cli = typer.Typer()
@@ -10,7 +10,6 @@ split_cli = typer.Typer()
 @split_cli.command()
 def start(
         dataset: str = "dataset.csv",
-        val_size: float = 0.0,
         test_size: float = 0.2,
         output_dir: str = "data/split"):
     """
@@ -25,17 +24,12 @@ def start(
     print(f"Splitting the dataset {dataset} saving to {output_dir}...")
     df = pd.read_csv(dataset)
     data = df.values
-    train_data, val_data, test_data = train_val_test_split(data, val_size, test_size, shuffle=False)
+    train_data, test_data = train_test_split(data, test_size, shuffle=False)
 
     # Save the train and validation data as npy files
     np.savez(
         os.path.join(output_dir, "train_data.npz"),
         data=train_data,
-        columns=df.columns.to_numpy()
-    )
-    np.savez(
-        os.path.join(output_dir, "val_data.npz"),
-        data=val_data,
         columns=df.columns.to_numpy()
     )
     np.savez(
@@ -46,10 +40,8 @@ def start(
     print(f"Train and validation data saved to {output_dir}.")
     print(f"Original data shape: {data.shape}")
     print(f"Train data shape: {train_data.shape}")
-    print(f"Validation data shape: {val_data.shape}")
     print(f"Test data shape: {test_data.shape}")
     print(f"Train data output path: {os.path.join(output_dir, 'train_data.npz')}")
-    print(f"Validation data output path: {os.path.join(output_dir, 'val_data.npz')}")
     print(f"Test data output path: {os.path.join(output_dir, 'test_data.npz')}")
 
 if __name__ == "__main__":
